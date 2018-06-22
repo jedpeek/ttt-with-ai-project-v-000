a73a643f315144cd885a1970078ee1d1fffa61bc
Jed Peek <jedpeek@yahoo.com>
2018-06-22 01:58:46 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,11 +2,11 @@ require 'player.rb'
 module Players
   class Computer < Player
 
-    def move(board)
-      move = rand(1..9).to_s until board.valid_move?(move)
-        move(board)
+      def move(board)
+        move = rand(1..9).to_s until board.valid_move?(move)
+          move(board)
+        end
+      move
     end
-    move
-  end
   end
 end
