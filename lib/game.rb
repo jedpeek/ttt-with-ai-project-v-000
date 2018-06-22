770b987b42987496890b78fce0edff76224ec088
Jed Peek <jedpeek@yahoo.com>
2018-06-22 00:48:01 +0000

    automatically backed up by learn

--- lib/game.rb
+++ lib/game.rb
@@ -94,4 +94,16 @@ def play(board)
     puts "Cat's Game!"
   end
 end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  user_input = gets.strip
+  index = input_to_index(user_input)
+  if valid_move?(board, index)
+     player = current_player(board)
+     move(board, index, player)
+     display_board(board)
+   else
+     turn(board)
+   end
 end
