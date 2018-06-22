b6e9a3548c385cf1d8422b346ad6f86dc4abcf0b
Jed Peek <jedpeek@yahoo.com>
2018-06-22 00:06:29 +0000

    automatically backed up by learn

---
+++ lib/game.rb
@@ -0,0 +1,43 @@
+class Game
+  attr_accessor :board, :player_1, :player_2
+    WIN_COMBINATIONS = [
+    [0,1,2], # Top row
+    [3,4,5], # Mid row
+    [6,7,8], # Btm row
+    [0,3,6], # L columns
+    [1,4,7], # M columns
+    [2,5,8], # R columns
+    [0,4,8], # R diagonal
+    [6,4,2]  # L diagonal
+    ]
+
+    def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board = Board.new)
+      @board
+      @player_1
+      @player_2
+    end
+
+    def board
+      @board
+    end
+
+    def player_1
+      @player_1
+    end
+    def player_2
+      @player_2
+    end
+
+    def current_player
+
+  count = 0
+  self.board.cells.each do |board|
+    if board != " "
+      count += 1
+    end
+    end
+  count % 2 == 0 ? "X" : "O"
+end
+
+
+end
---
+++ lib/players/human.rb
@@ -0,0 +1,12 @@
+require 'player.rb'
+module Players
+  class Human < Player
+
+    def move(board)
+      puts "Please enter 1-9: "
+      user_input = gets.strip
+    end
+
+  end
+
+end
