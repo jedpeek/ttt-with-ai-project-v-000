0b254ce7ed50015f58e98101a97386a1439d388f
Jed Peek <jedpeek@yahoo.com>
2018-06-22 01:57:08 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -3,7 +3,6 @@ module Players
   class Computer < Player
 
     def move(board)
-      #move = [1,2,3,4,5,6,7,8,9].detect{|i| !board.taken?(i)}.to_s
       move = rand(1..9).to_s
       until board.valid_move?(move)
         move(board)
